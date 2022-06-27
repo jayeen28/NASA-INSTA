b2a64383e1a81ab4ddd632ce554fb266c7d41848
Jayeen28 <jayeen6783@gmail.com>
2022-06-28 00:17:06 +0600

    verify email code added to handleInstaPost function

--- src/helpers/handleInstaPost.js
+++ src/helpers/handleInstaPost.js
@@ -1,41 +1,81 @@
+const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve(''), ms))
 const { INSTA_USER, INSTA_PASS } = process.env;
 const Instagram = require('instagram-web-api');
 const FileCookieStore = require('tough-cookie-filestore2');
 const getCode = require('./getCode');
 const cookieStore = new FileCookieStore('./cookies.json');
-const client = new Instagram({ username: INSTA_USER, password: INSTA_PASS, cookieStore });
 
 const clientLogin = async (client) => {
     const res = await new Promise(async (resolve, reject) => {
         try {
             const res = await client.login();
-            if (res.status === 'ok') return resolve(res.status)
+            if (res.authenticated) resolve(res.authenticated)
         }
         catch (err) {
-            console.log(err)
-            if (err.error?.message === 'checkpoint_required') {
-                const challengeUrl = err.error.checkpoint_url
-                const res = await client.updateChallenge({ challengeUrl, choice: 1 })
-                if (res.status === 'ok') resolve(res.status)
+            try {
+                if (err.error?.message === 'checkpoint_required') {
+                    const challengeUrl = err.error.checkpoint_url
+                    const res = await client.updateChallenge({ challengeUrl, choice: 1 })
+                    if (res.challengeType === 'VerifyEmailCodeForm') {
+                        await sleep(10000)
+                        const code = await getCode();
+                        console.log({ code1: code })
+                        const { navigation: { forward } } = res;
+                        if (code) {
+                            const res = await client.updateChallenge({ challengeUrl: `${forward}`, choice: 0, securityCode: code })
+                            console.log(res)
+                            if (res.status === 'ok') resolve(res)
+                        }
+                    }
+                    else resolve();
+                }
+            }
+            catch (err) {
+                if (err.error?.challenge?.challengeType === 'VerifyEmailCodeForm') {
+                    await sleep(10000)
+                    const code = await getCode();
+                    console.log({ code2: code })
+                    const { options: { uri } } = err;
+                    console.log(err)
+                    if (code) {
+                        const res = await client.updateChallenge({ challengeUrl: `${uri}`, choice: 0, securityCode: code })
+                        if (res.status === 'ok') resolve(res)
+                    }
+                }
             }
         }
     })
-    if (res === 'ok') return true;
+    if (res) return true;
     return false;
+}
+
+const upload = async (client, data) => {
+    const caption = `Title: ${data.title} \n\nExplaination: ${data.explanation} \n\n\n\nData collected from NASA and posted by MAD FALCON [bot]`;
+    const { media } = await client.uploadPhoto({ photo: 'resizedNewImage.jpg', caption });
+    console.log('[+] Image uploaded to instagram.');
+    return `[+] Post url: https://www.instagram.com/p/${media.code}/`
 }
 
 module.exports = {
     handleInstaPost: async (data) => {
+        const client = new Instagram({ username: INSTA_USER, password: INSTA_PASS, cookieStore });
         try {
             const res = await clientLogin(client);
             if (!res) return console.log(`[+] Login unsuccessful.`)
-            const caption = `Title: ${data.title} \n\nExplaination: ${data.explanation} \n\n\n\nData collected from NASA and posted by MAD FALCON [bot]`;
-            const { media } = await client.uploadPhoto({ photo: 'resizedNewImage.jpg', caption });
-            console.log('[+] Image uploaded to instagram.');
-            console.log(`[+] Post url: https://www.instagram.com/p/${media.code}/`);
+            const uploadRes = await upload(client, data);
+            console.log(uploadRes)
         }
         catch (err) {
-            console.error(`[-] ${err.message}`);
+            if (err.statusCode === 403) {
+                const res = await clientLogin(client);
+                if (!res) return console.log(`[+] Login unsuccessful.`)
+                const uploadRes = await upload(client, data);
+                console.log(uploadRes)
+            }
+            else console.error(`[-] ${err.message}`);
         }
     }
 }
+
+
+
